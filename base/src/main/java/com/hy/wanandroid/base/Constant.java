09efa718ee1b9ddb704ed9bd889db0594c542639
huangyong <756655135@qq.com>
2020-03-19 18:44:23 +0800

    依赖升级

---
+++ base/src/main/java/com/hy/wanandroid/base/Constant.java
@@ -0,0 +1,33 @@
+package com.hy.wanandroid.base;
+
+/**
+ * Created by huangyong on 2018/6/4
+ * 项目中用到的一些常量保存
+ */
+public class Constant {
+    private Constant() {
+    }
+
+    /**
+     * fragment arguments参数标识
+     */
+    public static final String ARG_PARAM1 = "param1";
+    public static final String ARG_PARAM2 = "param2";
+
+    /**
+     * 界面跳转传值标识
+     */
+    public static final String TYPE = "type";
+
+    /**
+     * 界面跳转传值类型
+     */
+    public static final String TYPE_BASIC_FRAGMENT = "type_record_video";
+
+
+    public static final String TYPE_COMMON_BUNDLE = "type_common_bundle";
+
+    //权限请求
+    public static final int REQUEST_PERMISSION_CODE = 0x100;
+
+}
---
+++ base/src/main/java/com/hy/wanandroid/base/FileUtils.java
@@ -0,0 +1,112 @@
+package com.hy.wanandroid.base;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by huangyong on 2018/3/14.
+ * 文件操作相关工具类
+ */
+
+public final class FileUtils {
+    private FileUtils() {
+    }
+
+    /**
+     * 判断文件是否存在
+     *
+     * @param filePath 文件路径
+     * @return {@code true}: 存在<br>{@code false}: 不存在
+     */
+    public static boolean isFileExists(final String filePath) {
+        return isFileExists(getFileByPath(filePath));
+    }
+
+    /**
+     * 判断文件是否存在
+     *
+     * @param file 文件
+     * @return {@code true}: 存在<br>{@code false}: 不存在
+     */
+    public static boolean isFileExists(final File file) {
+        return file != null && file.exists();
+    }
+
+    /**
+     * 按照文件路径返回文件
+     *
+     * @param filePath 文件路径
+     * @return 文件
+     */
+    public static File getFileByPath(final String filePath) {
+        return isSpace(filePath) ? null : new File(filePath);
+    }
+
+    /**
+     * Create a directory if it doesn't exist, otherwise do nothing.
+     *
+     * @param dirPath The path of directory.
+     * @return {@code true}: exists or creates successfully<br>{@code false}: otherwise
+     */
+    public static boolean createOrExistsDir(final String dirPath) {
+        return createOrExistsDir(getFileByPath(dirPath));
+    }
+
+    /**
+     * Create a directory if it doesn't exist, otherwise do nothing.
+     *
+     * @param file The file.
+     * @return {@code true}: exists or creates successfully<br>{@code false}: otherwise
+     */
+    public static boolean createOrExistsDir(final File file) {
+        return file != null && (file.exists() ? file.isDirectory() : file.mkdirs());
+    }
+
+    /**
+     * Create a file if it doesn't exist, otherwise do nothing.
+     *
+     * @param filePath The path of file.
+     * @return {@code true}: exists or creates successfully<br>{@code false}: otherwise
+     */
+    public static boolean createOrExistsFile(final String filePath) {
+        return createOrExistsFile(getFileByPath(filePath));
+    }
+
+    /**
+     * Create a file if it doesn't exist, otherwise do nothing.
+     *
+     * @param file The file.
+     * @return {@code true}: exists or creates successfully<br>{@code false}: otherwise
+     */
+    public static boolean createOrExistsFile(final File file) {
+        if (file == null) return false;
+        if (file.exists()) return file.isFile();
+        if (!createOrExistsDir(file.getParentFile())) return false;
+        try {
+            return file.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
+     * 删除文件
+     *
+     * @param file 待删除文件
+     * @return {@code true}: success<br>{@code false}: fail
+     */
+    public static boolean deleteFile(final File file) {
+        return file != null && (!file.exists() || file.isFile() && file.delete());
+    }
+
+    private static boolean isSpace(final String s) {
+        if (s == null) return true;
+        for (int i = 0, len = s.length(); i < len; ++i) {
+            if (!Character.isWhitespace(s.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
